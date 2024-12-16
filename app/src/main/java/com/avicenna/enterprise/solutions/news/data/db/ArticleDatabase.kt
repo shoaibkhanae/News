99a6f68ba67d480e056ae1869bd114d5d39d93a5
Shoaib Khan <shoaibae3@gmail.com>
2024-12-16 10:10:03 +0500

    Enabled Proper edge to edge

--- app/src/main/java/com/avicenna/enterprise/solutions/news/data/db/ArticleDatabase.kt
+++ app/src/main/java/com/avicenna/enterprise/solutions/news/data/db/ArticleDatabase.kt
@@ -1,8 +1,6 @@
 package com.avicenna.enterprise.solutions.news.data.db
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.avicenna.enterprise.solutions.news.data.models.Article
@@ -14,24 +12,5 @@ import com.avicenna.enterprise.solutions.news.data.models.Article
 )
 @TypeConverters(Converters::class)
 abstract class ArticleDatabase: RoomDatabase() {
-
     abstract fun getArticleDao(): ArticleDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: ArticleDatabase? = null
-
-        fun getDatabase(context: Context): ArticleDatabase {
-            return INSTANCE ?: synchronized(this) {
-                Room.databaseBuilder(
-                    context,
-                    ArticleDatabase::class.java,
-                    "articles_db"
-                )
-                    .fallbackToDestructiveMigration()
-                    .build()
-                    .also { INSTANCE = it }
-            }
-        }
-    }
 }
--- app/src/main/java/com/avicenna/enterprise/solutions/news/ui/activities/MainActivity.kt
+++ app/src/main/java/com/avicenna/enterprise/solutions/news/ui/activities/MainActivity.kt
@@ -1,13 +1,15 @@
-package com.avicenna.enterprise.solutions.news
+package com.avicenna.enterprise.solutions.news.ui.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.avicenna.enterprise.solutions.news.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,14 +17,17 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
             WindowInsetsCompat.CONSUMED
         }
+
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
